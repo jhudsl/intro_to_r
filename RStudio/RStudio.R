717d1a315ea7a4ff79f044a1aa165192e27f0ba2
muschellij2 <muschellij2@gmail.com>
2017-06-16 15:05:59 -0400

    fixed rstudio graphics

--- RStudio/RStudio.R
+++ RStudio/RStudio.R
@@ -2,6 +2,15 @@
 library(knitr)
 opts_chunk$set(comment = "")
 
+## ---- out.width = "90%", echo = FALSE------------------------------------
+knitr::include_graphics("rstudio_console.png")
+
 ## ---- out.width = "500px", echo = FALSE----------------------------------
 knitr::include_graphics("pane_layout.png")
+
+## ---- out.width = "90%", echo = FALSE------------------------------------
+knitr::include_graphics("rstudio_script.png")
+
+## ---- out.width = "90%", echo = FALSE------------------------------------
+knitr::include_graphics("rstudio_environment.png")
 
