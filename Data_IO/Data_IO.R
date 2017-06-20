41f15980c7770075224006589219107e78f9b9d4
muschellij2 <muschellij2@gmail.com>
2017-06-20 10:23:42 -0400

    updated saving

--- Data_IO/Data_IO.R
+++ Data_IO/Data_IO.R
@@ -45,3 +45,27 @@ names(dat)
 ## names(dat)[1] = "Year"
 ## write_csv(dat, path="YouthTobacco_newNames.csv")
 
+## ---- message = FALSE----------------------------------------------------
+x = 5;
+yts = readr::read_csv("http://johnmuschelli.com/intro_to_r/data/Youth_Tobacco_Survey_YTS_Data.csv")
+save(yts, x, file = "yts_data.rda")
+
+## ------------------------------------------------------------------------
+ls() # list things in the workspace
+rm(list = c("x", "yts"))
+z = load("yts_data.rda")
+ls()
+
+## ------------------------------------------------------------------------
+print(z)
+
+## ------------------------------------------------------------------------
+saveRDS(yts, file = "yts_dataset.rds")
+
+## ------------------------------------------------------------------------
+yts2 = readRDS(file = "yts_dataset.rds")
+identical(yts, yts2) # test if they are the same 
+
+## ---- echo = FALSE, message = FALSE--------------------------------------
+file.remove("yts_data.rda") # removing temporary file
+
