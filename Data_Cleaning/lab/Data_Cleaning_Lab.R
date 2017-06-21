aaf5ef61cc4f6ac970ecf325f6a53adb04269261
muschellij2 <muschellij2@gmail.com>
2017-06-21 19:12:36 -0400

    updated homework 3

--- Data_Cleaning/lab/Data_Cleaning_Lab.R
+++ Data_Cleaning/lab/Data_Cleaning_Lab.R
@@ -10,56 +10,80 @@
 
 # 2. How many addresses pay property taxes? 
 
-# 3. What is the total city and state tax paid?
+# 3. What is the total city and state tax paid?  
+# You need to remove the $ from the CityTax variable
+# then you need to make it numeric.   Try str_replace, but remember
+# $ is "special" and you need fixed() around it.
 
-# 4. Using `table()` or group_by and summarize(n())
+# 4. Using `table()` or group_by and summarize(n()) or tally()
 #	a. how many observations/properties are in each ward?
 
-#	b. what is the mean state tax per ward? use group_by
+#	b. what is the mean state tax per ward? use group_by and summarize
 
-#	c. what is the maximum amount still due in each ward?
+#	c. what is the maximum amount still due in each ward?  different summarization (max)
 
 # d. What is the 75th percentile of city and state tax paid by Ward? (quantile)
 
-# 6. Make boxplots using base graphics showing cityTax 
-#	 	by whether the property	is a principal residence or not.
+# 6. Make boxplots using base graphics showing cityTax (y -variable)
+#	 	by whether the property	is a principal residence (x) or not.
 
 # 7. Subset the data to only retain those houses that are principal residences. 
+# which command subsets rows? Filter or select?
 #	a) How many such houses are there?
 
-#	b) Describe the distribution of property taxes on these residences.
+#	b) Describe the distribution of property taxes on these residences.  Use 
+# hist with certain breaks or plot(density(variable))
 
-# 8. Convert the 'lotSize' variable to a numeric square feet variable. 
-#	Tips: - Assume hyphens represent decimal places within measurements. 
-#		  - 1 acre = 43560 square feet
-#		  - The hyphens represent inches (not decimals)
-# 		  - Don't spend more than 5-10 minutes on this; stop and move on
-
-# 9.a) Plot your numeric lotSize versus cityTax on principal residences. 
+# 8.a) Plot your numeric lotSize versus cityTax on principal residences. 
 #	b) How many values of lot size were missing?
+
 
 
 ################################
 ## Read in the Salary FY2015 dataset
-
 # http://johnmuschelli.com/intro_to_r/data/Baltimore_City_Employee_Salaries_FY2015.csv
 
 # 10. Make an object called health.sal using the salaries data set, 
 #		with only agencies of those with "fire" (or any forms), if any, in the name
+# remember fixed( ignore_case = TRUE) will ignore cases
 
 # 11. Make a data set called trans which contains only agencies that contain "TRANS".
 
-# 12. What is/are the profession(s) of people who have "abra" in their name for Baltimore's Salaries?
+# 12. What is/are the profession(s) of people who have "abra" in their name for 
+# Baltimore's Salaries?  Case should be ignored
 
-# 13. What is the distribution of annual salaries look like? What is the IQR?
+# 13. What is the distribution of annual salaries look like? (use hist, 20 breaks) What is the IQR?
+#Hint: first convert to numeric. Try str_replace, but remember
+# $ is "special" and you need fixed() around it.
 
-# 14. Convert HireDate to the `Date` class - plot Annual Salary vs Hire Date
 
-# 15. Plot annual salary versus hire date. 
-#		Hint: first convert to numeric and date respectively
+# 14. Convert HireDate to the `Date` class - plot Annual Salary vs Hire Date.
+# Use AnnualSalary ~ HireDate with a data = sal argument in plot or use 
+# x, y notation in scatter.smooth
+# Use lubridate package.  Is it mdy(date) or dmy(date) for this data - look at HireDate
 
-# 16. Create a smaller dataset that only includes the
-# 	Police Department,  Fire Department and Sheriff's Office.
+
+# 15. Create a smaller dataset that only includes the
+# 	Police Department,  Fire Department and Sheriff's Office.  Use the Agency variable
+# with string matching Call this emer
 #  a. How many employees are in this new dataset?
 
-# 17. Replot annual salary versus hire date, color by Agency using base plotting 
+
+# 16. Create a varaible called dept in the emer data set.
+# dept = str_extract(Agency, ".*(ment|ice)").  Ee want to extract all characters
+# up until ment or ice (we can group in regex using parentheses) and then discard
+# the rest.
+
+
+# Replot annual salary versus hire date, color by dept (not yet - using ggplot)
+ggplot(aes(x = HireDate, y = AnnualSalary, 
+           colour = dept), data = emer) + 
+  geom_point() + theme(legend.position = c(0.5, 0.8))
+
+
+
+# BONUS. Convert the 'LotSize' variable to a numeric square feet variable. 
+# Using the tax data set
+#	Tips: - 1 acre = 43560 square feet
+#		    - The hyphens represent inches (not decimals)
+# 		  - Don't spend more than 5-10 minutes on this; stop and move on
--- Data_Cleaning/lab/Data_Cleaning_Lab_key.R
+++ Data_Cleaning/lab/Data_Cleaning_Lab_key.R
@@ -9,6 +9,7 @@ rm( list = ls() ) # clear the workspace
 library(stringr)
 library(dplyr)
 library(readr)
+library(lubridate)
 
 # 1. Read the Property Tax data into R and call it the variable `tax`
 tax = read_csv( "http://johnmuschelli.com/intro_to_r/data/Real_Property_Taxes.csv.gz")
@@ -17,7 +18,11 @@ tax = read_csv( "http://johnmuschelli.com/intro_to_r/data/Real_Property_Taxes.cs
 nrow(tax)
 dim(tax)
 
-# 3. What is the total city and state tax paid?
+# 3. What is the total city and state tax paid?  
+# You need to remove the $ from the CityTax variable
+# then you need to make it numeric.   Try str_replace, but remember
+# $ is "special" and you need fixed() around it.
+
 head(tax$CityTax)
 tax$CityTax = tax$CityTax %>% 
   str_replace(fixed("$"), "") %>%
@@ -34,20 +39,21 @@ sum(tax$StateTax, na.rm = TRUE)
 sum(tax$StateTax, na.rm = TRUE)/1e6
 
 
-# Using `tapply()` and/or `table()`
-#	a. how many observations are in each ward?
+# 4. Using `table()` or group_by and summarize(n()) or tally()
+#	a. how many observations/properties are in each ward?
 ### sapply(tax_list, nrow)
-
 table(tax$Ward)
 
 
-#	b. what is the mean state tax per ward?
+
+#	b. what is the mean state tax per ward? use group_by and summarize
 tax %>%   group_by(Ward) %>% 
   summarize(mean_state = mean(StateTax, na.rm = TRUE))
 
 tapply(tax$StateTax, tax$Ward, mean, na.rm=TRUE)
 
-#	c. what is the maximum amount still due in each ward?
+
+#	c. what is the maximum amount still due in each ward?  different summarization (max)
 tax$AmountDue = tax$AmountDue %>% 
   str_replace(fixed("$"), "") %>%
   as.numeric
@@ -55,7 +61,7 @@ tax$AmountDue = tax$AmountDue %>%
 tax %>% group_by(Ward) %>% 
   summarize(maxDue = max(AmountDue, na.rm = TRUE))
 
-## 75th percentile
+# d. What is the 75th percentile of city and state tax paid by Ward? (quantile)
 tax %>% group_by(Ward) %>% 
   summarize(Percentile = quantile(StateTax, prob = 0.75,na.rm = TRUE))
 
@@ -64,22 +70,93 @@ tapply(tax$StateTax, tax$Ward,
 tapply(tax$StateTax, tax$Ward,
        quantile, na.rm=TRUE)
 
-# 6. Make boxplots using base graphics showing CityTax 
-#	 	by whether the property	is a principal residence or not.
+# 6. Make boxplots using base graphics showing cityTax (y -variable)
+#	 	by whether the property	is a principal residence (x) or not.
 tax$ResCode = str_trim(tax$ResCode)
 boxplot(log(CityTax+1) ~ ResCode, data = tax)
 
 # 7. Subset the data to only retain those houses that are principal residences. 
+# which command subsets rows? Filter or select?
+#	a) How many such houses are there?
 pres = tax %>% filter( ResCode %in% "PRINCIPAL RESIDENCE")
 pres = tax %>% filter( ResCode == "PRINCIPAL RESIDENCE")
 
 #	a) How many such houses are there?
 dim(pres)
 
-#	b) Describe the distribution of property taxes on these residences.
+#	b) Describe the distribution of property taxes on these residences.  Use 
+# hist with certain breaks or plot(density(variable))
 hist(log2(pres$CityTax+1))
+hist(pres$CityTax)
+plot(density(pres$CityTax))
 
-# 8. Convert the 'LotSize' variable to a numeric square feet variable. 
+
+
+################################
+## Read in the Salary FY2015 dataset
+# http://johnmuschelli.com/intro_to_r/data/Baltimore_City_Employee_Salaries_FY2015.csv
+
+# 10. Make an object called health.sal using the salaries data set, 
+#		with only agencies of those with "fire" (or any forms), if any, in the name
+# remember fixed( ignore_case = TRUE) will ignore cases
+sal = read_csv("http://johnmuschelli.com/intro_to_r/data/Baltimore_City_Employee_Salaries_FY2015.csv")
+health.sal = sal %>% filter(str_detect(JobTitle, fixed("fire", ignore_case = TRUE)))
+
+# 11. Make a data set called trans which contains only agencies that contain "TRANS".
+trans = sal %>% filter(str_detect(JobTitle, "TRANS"))
+
+# 12. What is/are the profession(s) of people who have "abra" in their name for 
+# Baltimore's Salaries?  Case should be ignored
+sal %>% filter(str_detect(name, fixed("abra", ignore_case = TRUE)))
+
+# 13. What is the distribution of annual salaries look like? (use hist, 20 breaks) What is the IQR?
+#Hint: first convert to numeric. Try str_replace, but remember
+# $ is "special" and you need fixed() around it.
+sal = sal %>% mutate(AnnualSalary = str_replace(AnnualSalary, fixed("$"), ""))
+sal = sal %>% mutate(AnnualSalary = as.numeric(AnnualSalary))
+hist(sal$AnnualSalary, breaks = 20)
+
+# 14. Convert HireDate to the `Date` class - plot Annual Salary vs Hire Date.
+# Use AnnualSalary ~ HireDate with a data = sal argument in plot or use 
+# x, y notation in scatter.smooth
+# Use lubridate package.  Is it mdy(date) or dmy(date) for this data - look at HireDate
+sal = sal %>% mutate(HireDate = lubridate::mdy(HireDate))
+plot(AnnualSalary ~ HireDate, data = sal)
+scatter.smooth(sal$AnnualSalary, x = sal$HireDate, col = "red")
+
+# 15. Create a smaller dataset that only includes the
+# 	Police Department,  Fire Department and Sheriff's Office.  Use the Agency variable
+# with string matching Call this emer
+#  a. How many employees are in this new dataset?
+emer = sal %>% filter(
+  str_detect(Agency, "Sheriff's Office|Police Department|Fire Department")
+)
+
+emer = sal %>% filter(
+  str_detect(Agency, "Sheriff's Office") |
+    str_detect(Agency, "Police Department") |
+    str_detect(Agency, "Fire Department")
+)
+
+# 16. Create a varaible called dept in the emer data set.
+# dept = str_extract(Agency, ".*(ment|ice)").  Ee want to extract all characters
+# up until ment or ice (we can group in regex using parentheses) and then discard
+# the rest.
+emer = emer %>% 
+  mutate(
+    dept = str_extract(Agency, ".*(ment|ice)")
+  )
+# Replot annual salary versus hire date, color by dept (not yet - using ggplot)
+ggplot(aes(x = HireDate, y = AnnualSalary, 
+           colour = dept), data = emer) + 
+  geom_point() + theme(legend.position = c(0.5, 0.8))
+
+
+
+
+
+# BONUS. Convert the 'LotSize' variable to a numeric square feet variable. 
+# Using the tax data set
 #	Tips: - 1 acre = 43560 square feet
 #		    - The hyphens represent inches (not decimals)
 # 		  - Don't spend more than 5-10 minutes on this; stop and move on
@@ -88,8 +165,8 @@ tax$LotSize = str_trim(tax$LotSize) # trim to be safe
 lot = tax$LotSize # for checking later
 
 # first lets take care of acres
-aIndex= c(grep("ACRE.*", tax$LotSize,ignore.case),
-            grep(" %", tax$LotSize, fixed=TRUE))
+aIndex= c(grep("ACRE.*", tax$LotSize, ignore.case = TRUE),
+          grep(" %", tax$LotSize, fixed=TRUE))
 head(aIndex)
 head(lot[aIndex])
 
@@ -172,31 +249,3 @@ table(is.na(tax$sqft))
 
 #what remains?
 lot[is.na(tax$sqft)]
-
-
-
-
-################################
-## Read in the Salary FY2015 dataset
-
-# http://johnmuschelli.com/intro_to_r/data/Baltimore_City_Employee_Salaries_FY2015.csv
-
-# 10. Make an object called health.sal using the salaries data set, 
-#		with only agencies of those with "fire" (or any forms), if any, in the name
-
-# 11. Make a data set called trans which contains only agencies that contain "TRANS".
-
-# 12. What is/are the profession(s) of people who have "abra" in their name for Baltimore's Salaries?
-
-# 13. What is the distribution of annual salaries look like? What is the IQR?
-
-# 14. Convert HireDate to the `Date` class - plot Annual Salary vs Hire Date
-
-# 15. Plot annual salary versus hire date. 
-#		Hint: first convert to numeric and date respectively
-
-# 16. Create a smaller dataset that only includes the
-# 	Police Department,  Fire Department and Sheriff's Office.
-#  a. How many employees are in this new dataset?
-
-# 17. Replot annual salary versus hire date, color by Agency using base plotting 
