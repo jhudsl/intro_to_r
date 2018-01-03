27054b7423467994aa914068f13a39345767edd4
andrewejaffe <andrewejaffe@gmail.com>
2018-01-02 21:48:44 -0500

    updating labs

--- Manipulating_Data_in_R/lab/Manipulating_Data_in_R_Lab.R
+++ Manipulating_Data_in_R/lab/Manipulating_Data_in_R_Lab.R
@@ -2,27 +2,17 @@
 ## Data Manipulation Lab 
 ############
 
-library(readr)
-library(dplyr)
-library(ggplot2)
-library(tidyr)
-library(stringr)
-
 #############################
 # Part 1
 #############################
 # 1.  Read in the Wide Bike lanes data: 
 # http://johnmuschelli.com/intro_to_r/data/Bike_Lanes_Wide.csv 
 # Name the data set wide
-wide = read_csv("http://johnmuschelli.com/intro_to_r/data/Bike_Lanes_Wide.csv")
 
 # 2. Reshape wide using gather.  Call this data long.  Make the key
 # lanetype, and the value the_length.  Make sure we gather all columns **but**
 # name, using -name.
-long = gather(wide, key = "lanetype", 
-              value = "the_length",
-              -name)
-long = filter(long, !is.na(the_length))
+
 # note the NAs here
 
 
@@ -36,37 +26,19 @@ long = filter(long, !is.na(the_length))
 ## 3. read in the roads and crashes CSVs.  Call them crash and road
 # crash: http://johnmuschelli.com/intro_to_r/Manipulating_Data_in_R/lab/crashes.csv
 # road: http://johnmuschelli.com/intro_to_r/Manipulating_Data_in_R/lab/roads.csv
-crash = read_csv("http://johnmuschelli.com/intro_to_r/Manipulating_Data_in_R/lab/crashes.csv")
-road = read_csv("http://johnmuschelli.com/intro_to_r/Manipulating_Data_in_R/lab/roads.csv")
 
 # run head(crash), head(road)
-head(crash)
-
-head(road)
 
 # 4.  Replace (using str_replace) any hyphens (-) with a space in crash$Road.
 # Table the road variable.  Call this data crash2
-crash2 = crash %>% 
-  mutate(Road = str_replace(Road, "-", " ")
-  )
-
-table(crash2$Road)
 
 ## 5. How many observations are in each dataset?
-nrow(crash)
-nrow(road)
 
 # 6.  Separate the Road column (using `separate`) into (type and number) in
 # crash2.  Reassign this to crash2. Table crash2$type
-crash2 = crash2 %>% 
-  separate(Road, into = c("type", "number"))
-table(crash2$type)
 
 # Create a new variable calling it road_hyphen usin the unite function.  Unite the
 # type and number columns using a hyphen (-) and then table road_hyphen
-crash2 = crash2 %>% 
-  unite(col = road_hyphen, type, number, sep = "-")
-table(crash2$road_hyphen)
 
 ## 7. Which and how many years were data collected?
 
@@ -76,7 +48,6 @@ table(crash2$road_hyphen)
 #############################
 # 8.  Read in the Bike lanes data: http://johnmuschelli.com/intro_to_r/data/Bike_Lanes.csv 
 # Name the data set bike
-bike = read_csv("http://johnmuschelli.com/intro_to_r/data/Bike_Lanes.csv")
   
 # 9.  Filter any rows that have a missing value in the "type" column or missing 
 # name and re-assign the output to bike
@@ -98,12 +69,6 @@ bike = read_csv("http://johnmuschelli.com/intro_to_r/data/Bike_Lanes.csv")
 ##	e.g. those observations with road lengths and districts.
 # Merge without using `by` argument, then merge using `by = "Road"`.
 # call the output merged.  How many observations are there?
-merged = inner_join(road, crash)
-
-merged = inner_join(road, crash, by = "Road")
-
-anti = anti_join(road, crash, by = "Road")
-anti2 = anti_join(crash, road, by = "Road")
 
 ## 13. Join data using a full_join.  Call the output full. 
 # How many observations are there?
--- Statistics/lab/Statistics_Lab.R
+++ Statistics/lab/Statistics_Lab.R
@@ -2,46 +2,24 @@
 ## Statistics Lab 
 ############
 
-library(readr)
-library(dplyr)
-library(ggplot2)
-library(tidyr)
-library(stringr)
-library(psych)
-library(grid)
-library(broom)
-
 #############################
 # Part 1
 #############################
 # Read in the following infant mortality data: 
 # http://johnmuschelli.com/intro_to_r/data/indicatordeadkids35.csv
-mort = read_csv("http://johnmuschelli.com/intro_to_r/data/indicatordeadkids35.csv")
-colnames(mort)[1] = "country"
 
 # 1. Compute the correlation between the 1980, 1990, 2000, and 2010 mortality data.
 #    No need to save this in an object. Just display the result to the screen.
-mort[, c("1980", "1990", "2010")]
-mort %>% select(`1980`, `1990`, `2000`, "2010") %>% cor(use = "complete.obs")
-
-mort %>% select(`1980`, `1990`, `2000`, "2010") %>% pysch::corr.test(adjust = "none")
-
 
 # 2. a. Compute the correlation between the Myanmar, China, and United States mortality data.
 #       Store this correlation matrix in an object called country_cor
 #    b. Extract the Myanmar-US correlation from the correlation matrix.
 
-
-
-
 # 3. Is there a difference between mortality information from 1990 and 2000?
 #    Run a t-test and a Wilcoxon rank sum test to assess this.
 #    Hint: to extract the column of information for 1990, use mort[["1990"]]
 
-
-
-
-#############################
+#################
 # Part 2
 #############################
 # Read in the Kaggle cars auction dataset:
@@ -51,23 +29,13 @@ cars = read_csv("http://johnmuschelli.com/intro_to_r/data/kaggleCarAuction.csv")
 #    vehicle age (VehicleAge) and size (Size) as predictors as well as their interaction.
 #    Save the model fit in an object called lmfit_cars and display the summary table.
 
-
-
-
 # 5. Create a variable called "expensive" in the cars data that indicates if the 
 #    vehicle cost is over $80,000. Use a chi-squared test to assess if there is a
 #    relationship between a car being expensive and it being labeled as a "bad buy."
 
-
-
-
 # 6. Fit a logistic regression model where the outcome is "bad buy" status and predictors
 #    are the "expensive" status and vehicle age (VehicleAge).
 #    Save the model fit in an object called logfit_cars and display the summary table.
-
-
-
-
 
 #############################
 # Part 3
@@ -77,11 +45,6 @@ cars = read_csv("http://johnmuschelli.com/intro_to_r/data/kaggleCarAuction.csv")
 #    and store this new dataset in an object called cars_subsample.
 
 
-
-
 # 8. Fit the same logistic regression model as in problem 6 above 
 # with this sample and display the 
 #    summary table. How do the results compare?
-
-
-
