fbe8b39cd410c9db18a66c977e279644df1375e2
andrewejaffe <andrewejaffe@gmail.com>
2018-06-11 08:41:45 -0400

    backtick lab 1

--- Basic_R/lab/Basic_R_Lab.R
+++ Basic_R/lab/Basic_R_Lab.R
@@ -10,32 +10,32 @@
 #######################################
 # Part 1
 #######################################
-# 1. create a new variable called 'my.num' that contains 6 numbers
+# 1. create a new variable called `my.num` that contains 6 numbers
 
-# 2. mulitply 'my.num' by 4
+# 2. mulitply `my.num` by 4
 
-# 3. create a second variable called 'my.char' that contains 5 character strings
+# 3. create a second variable called `my.char` that contains 5 character strings
 
-# 4. combine the two variables 'my.num' and 'my.char' into a variable called 'both'
+# 4. combine the two variables `my.num` and `my.char` into a variable called `both`
 
-# 5. what is the length of 'both'?
+# 5. what is the length of `both`?
 
-# 6. what class is 'both'?
+# 6. what class is `both`?
 
 #######################################
 # Part 2
 #######################################
-# 7. divide 'both' by 3, what happens?
+# 7. divide `both` by 3, what happens?
 
-# 8. create a vector with elements 1 2 3 4 5 6 and call it x
+# 8. create a vector with elements 1 2 3 4 5 6 and call it `x`
 
-# 9. create another vector with elements 10 20 30 40 50 and call it y
+# 9. create another vector with elements 10 20 30 40 50 and call it `y`
 
-# 10. what happens if you try to add x and y together? why?
+# 10. what happens if you try to add `x` and `y` together? why?
 
-# 11. append the value 60 onto the vector y (hint: you can use the c() function)
+# 11. append the value 60 onto the vector `y` (hint: you can use the `c()` function)
 
-# 12. add x and y together
+# 12. add `x` and `y` together
 
-# 13. multiply x and y together. pay attention to how R performs operations on vectors of the same length.
+# 13. multiply `x` and `y` together. pay attention to how R performs operations on vectors of the same length.
 
