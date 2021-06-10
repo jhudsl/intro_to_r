e620bc18198dd3d6de4818c8140050c5a04d9c65
Carrie Wright <cwrigh60@jhu.edu>
2021-06-10 18:08:20 -0400

    updating website material content with make in main dir

--- Intro/Intro.R
+++ Intro/Intro.R
@@ -4,26 +4,26 @@ opts_chunk$set(comment = "")
 
 
 ## ---- fig.alt="Welcome!", out.width = "60%", echo = FALSE, fig.align='center'----
-knitr::include_graphics("../images/welcome.jpg")
+knitr::include_graphics("images/welcome.jpg")
 
 
-## ---- fig.alt="Welcome!", out.width = "30%", echo = FALSE, fig.align='center'----
+## ---- fig.alt="Carrie's picture", out.width = "30%", echo = FALSE, fig.align='center'----
 knitr::include_graphics("https://ca.slack-edge.com/T023TPZA8LF-U024F9G49S8-9861ddd543db-192")
 
 
-## ---- fig.alt="Welcome!", out.width = "30%", echo = FALSE, fig.align='center'----
+## ---- fig.alt="Ava's picture", out.width = "30%", echo = FALSE, fig.align='center'----
 knitr::include_graphics("https://ca.slack-edge.com/T023TPZA8LF-U0248RUHRC2-0ac3f9608641-512")
 
 
-## ---- fig.alt="Welcome!", out.width = "30%", echo = FALSE, fig.align='center'----
+## ---- fig.alt="Marta's picture", out.width = "30%", echo = FALSE, fig.align='center'----
 knitr::include_graphics("https://ca.slack-edge.com/T023TPZA8LF-U024EN26Q0L-113294823b2c-512")
 
 
-## ---- fig.alt="Welcome!", out.width = "40%", echo = FALSE, fig.align='center'----
+## ---- fig.alt="Bell Labs old logo", out.width = "40%", echo = FALSE, fig.align='center'----
 knitr::include_graphics("https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Bell_Laboratories_logo.svg/2880px-Bell_Laboratories_logo.svg.png")
 
 
-## ---- fig.alt="Welcome!", out.width = "20%", echo = FALSE, fig.align='center'----
+## ---- fig.alt="R logo", out.width = "20%", echo = FALSE, fig.align='center'----
 knitr::include_graphics("https://www.r-project.org/Rlogo.png")
 
 
@@ -31,15 +31,15 @@ knitr::include_graphics("https://www.r-project.org/Rlogo.png")
 knitr::include_graphics("https://rladies-baltimore.github.io/img/R-LadiesGlobal_RBG_online_LogoOnly.png")
 
 
-## ---- fig.alt="Welcome!", out.width = "30%", echo = FALSE, fig.align='center'----
+## ---- fig.alt="R hex stickers for packages", out.width = "30%", echo = FALSE, fig.align='center'----
 knitr::include_graphics("../images/hex.png")
 
 
-## ---- fig.alt="Welcome!", out.width = "60%", echo = FALSE, fig.align='center'----
-knitr::include_graphics("../images/hope.jpg")
+## ---- fig.alt="image of rocks with word hope painted on", out.width = "60%", echo = FALSE, fig.align='center'----
+knitr::include_graphics("images/hope.jpg")
 
 
-## ---- fig.alt="Welcome!", out.width = "60%", echo = FALSE, fig.align='center'----
+## ---- fig.alt="Intro to R course logo", out.width = "60%", echo = FALSE, fig.align='center'----
 knitr::include_graphics("../images/Intro_to_R.png")
 
 
