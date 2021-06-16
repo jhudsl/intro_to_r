aba14bec2c778ea6ab61975e6ef28db954d843c4
Carrie Wright <cwrigh60@jhu.edu>
2021-06-15 23:51:26 -0400

    updating Data Cleaning (reducing) and rendering data classes, data summarization, and data cleaning

--- Data_Cleaning/index.R
+++ Data_Cleaning/index.R
@@ -12,17 +12,9 @@ x[2]
 
 
 ## -----------------------------------------------------------------------------
-nth(x, n = 2)
-
-
-## -----------------------------------------------------------------------------
 x = c(1, 2, 4, 8, 10)
 x[5]
 x[c(2,5)]
-
-
-## ---- error=TRUE--------------------------------------------------------------
-nth(x, n = c(2, 5)) # nth only returns one number
 
 
 ## ----negativeIndex------------------------------------------------------------
@@ -36,35 +28,24 @@ x[-(1:3)] # needs parentheses
 
 
 ## -----------------------------------------------------------------------------
-x
-x > 2
-x[ x > 2 ]
+complete.cases(x)
+complete.cases(mtcars)
+
+
+## ---- error=FALSE-------------------------------------------------------------
+#install.packageS(naniar)
+library(naniar)
+x = c(0, NA, 2, 3, 4, -0.5, 0.2)
+naniar::pct_complete(x)
 
 
 ## -----------------------------------------------------------------------------
-x[ x > 2 & x < 5 ]
-x[ x > 5 | x == 2 ]
-
-
-## -----------------------------------------------------------------------------
-which(x > 5 | x == 2) # returns index
-x[ which(x > 5 | x == 2) ]
-x[ x > 5 | x == 2 ]
+naniar::gg_miss_var(iris)
 
 
 ## -----------------------------------------------------------------------------
 x = c(0, NA, 2, 3, 4, -0.5, 0.2)
 x > 2
-
-
-## -----------------------------------------------------------------------------
-x != NA
-x > 2 & !is.na(x)
-
-
-## -----------------------------------------------------------------------------
-(x == 0 | x == 2) # has NA
-(x == 0 | x == 2) & !is.na(x) # No NA
 
 
 ## -----------------------------------------------------------------------------
@@ -74,8 +55,10 @@ df %>% filter(between(x, -1, 3) | is.na(x))
 
 
 ## -----------------------------------------------------------------------------
-x %in% c(0, 2, NA) # this 
-x %in% c(0, 2) | is.na(x) # versus this
+x # looks like the 1st and 3rd element should be TRUE
+x %in% c(0, 2) # uh oh - not good!
+x %in% c(0, 2) | is.na(x) # do this
+
 
 
 ## -----------------------------------------------------------------------------
@@ -98,54 +81,6 @@ tibble(x = c(0, 1, 2, 3, 2, 3, 3, 2, 2, 3)) %>%  count(x)
 ## ----onetab-------------------------------------------------------------------
 table(c(0, 1, 2, 3, 2, 3, 3, 2, 2, 3), 
         useNA = "always")
-
-
-## ----onetab_fact, error = TRUE------------------------------------------------
-fac = factor(c(0, 1, 2, 3, 2, 3, 3, 2,2, 3),
-             levels = 1:4)
-tab = table(fac)
-tab
-tab[ tab > 0 ]
-
-
-## -----------------------------------------------------------------------------
-tibble(x = fac) %>% count(x)
-
-
-## -----------------------------------------------------------------------------
-tab <- table(c(0, 1, 2, 3, 2, 3, 3, 2,2, 3), 
-             c(0, 1, 2, 3, 2, 3, 3, 4, 4, 3), 
-              useNA = "always")
-tab
-
-
-## -----------------------------------------------------------------------------
-tab_df = tibble(x = c(0, 1, 2, 3, 2, 3, 3, 2,2, 3), 
-             y = c(0, 1, 2, 3, 2, 3, 3, 4, 4, 3))
-tab_df %>% count(x, y)
-
-
-## ----margin-------------------------------------------------------------------
-tab
-margin.table(tab, 2)
-
-
-## ----table2-------------------------------------------------------------------
-prop.table(tab)
-prop.table(tab,1) * 100
-
-
-## -----------------------------------------------------------------------------
-tab_df %>% 
-  count(x, y) %>% 
-  group_by(x) %>% mutate(pct_x = n / sum(n))
-
-
-## ---- message = FALSE---------------------------------------------------------
-library(scales)
-tab_df %>% 
-  count(x, y) %>% 
-  group_by(x) %>% mutate(pct_x = percent(n / sum(n)))
 
 
 ## ----readSal, echo = TRUE, eval = FALSE---------------------------------------
@@ -174,40 +109,16 @@ any(is.na(Sal$Name)) # are there any NAs?
 
 ## ---- eval = FALSE------------------------------------------------------------
 ## data = data %>%
-##   mutate(gender = recode(gender, M = "Male", m = "Male", M = "Male"))
-
-
-## ---- eval = FALSE------------------------------------------------------------
-## data %>%
-##   mutate(gender = ifelse(gender %in% c("Male", "M", "m"),
-##                          "Male", gender))
-
-
-## ----gender, echo=FALSE-------------------------------------------------------
-set.seed(4) # random sample below - make sure same every time
-gender <- sample(c("Male", "mAle", "MaLe", "M", "MALE", "Ma", "FeMAle", "F", "Woman", "Man", "Fm", "FEMALE"), 1000, replace = TRUE)
-
-
-## ----gentab-------------------------------------------------------------------
-table(gender)
-
-
-## ----gender2, echo=FALSE------------------------------------------------------
-gender = gender %>% 
-  tolower %>% 
-  recode(m = "Male", f = "Female", ma = "Male",
-         woman = "Female", man = "Male")
-
-
-## ----gentab2------------------------------------------------------------------
-table(gender)
+##   mutate(gender = recode(gender, M = "Male", m = "Male", Man = "Male"))
 
 
 ## ----str_split_orig-----------------------------------------------------------
 library(stringr)
-x <- c("I really", "like writing", "R code programs")
-y <- str_split(x, " ") # returns a list
+x <- c("I really like writing R code")
+df = tibble(x = c("I really", "like writing", "R code programs"))
+y <- unlist(str_split(x, " "))
 y
+length(y)
 
 
 ## -----------------------------------------------------------------------------
@@ -217,9 +128,9 @@ str_split("I.like.strings", fixed("."))
 
 ## ----stsplit2-----------------------------------------------------------------
 y[[2]]
-sapply(y, dplyr::first) # on the fly
-sapply(y, nth, 2) # on the fly
-sapply(y, last) # on the fly
+# sapply(y, dplyr::first) # on the fly
+# sapply(y, nth, 2) # on the fly
+# sapply(y, last) # on the fly
 
 
 ## ----separate_df--------------------------------------------------------------
@@ -228,22 +139,6 @@ df = tibble(x = c("I really", "like writing", "R code programs"))
 
 ## -----------------------------------------------------------------------------
 df %>% separate(x, into = c("first", "second", "third"))
-
-
-## ----separate_df2_for_easy----------------------------------------------------
-df = tibble(x = c("I really", "like writing", "R code programs"))
-
-
-## -----------------------------------------------------------------------------
-df %>% separate(x, into = c("first", "second"))
-
-
-## ----separate_df2-------------------------------------------------------------
-df = tibble(x = c("I really", "like. _writing R. But not", "R code programs"))
-
-
-## -----------------------------------------------------------------------------
-df %>% separate(x, into = c("first", "second", "third"), extra = "merge")
 
 
 ## -----------------------------------------------------------------------------
@@ -255,18 +150,7 @@ df %>% separate(x, into = c("first", "second", "third"),
 head(str_detect(Sal$Name, "Rawlings"))
 
 
-## ----RawlMatch----------------------------------------------------------------
-which(str_detect(Sal$Name, "Rawlings"))
-
-
-## ----ggrep2-------------------------------------------------------------------
-ss = str_extract(Sal$Name, "Rawling")
-head(ss)
-ss[ !is.na(ss)]
-
-
-## ----ggrep--------------------------------------------------------------------
-str_subset(Sal$Name, "Rawlings")
+## -----------------------------------------------------------------------------
 Sal %>% filter(str_detect(Name, "Rawlings"))
 
 
@@ -280,66 +164,39 @@ head(str_subset( Sal$Name, "Spence.*C.*"))
 
 
 ## -----------------------------------------------------------------------------
-head(str_extract(Sal$AgencyID, "\\d"))
-head(str_extract_all(Sal$AgencyID, "\\d"), 2)
+head(Sal$Name, 2)
+head(str_replace(Sal$Name, "a", "j"),2)
 
 
 ## -----------------------------------------------------------------------------
-head(str_replace(Sal$Name, "a", "j"))
 head(str_replace_all(Sal$Name, "a", "j"), 2)
 
 
 ## ----classSal-----------------------------------------------------------------
 class(Sal$AnnualSalary)
 
+
 ## ----destringSal--------------------------------------------------------------
 head(Sal$AnnualSalary, 4)
 head(as.numeric(Sal$AnnualSalary), 4)
 
 
-## ----orderSal-----------------------------------------------------------------
-Sal = Sal %>% mutate(
-  AnnualSalary = str_replace(AnnualSalary, fixed("$"), ""),
-  AnnualSalary = as.numeric(AnnualSalary)
-  ) %>% 
-  arrange(desc(AnnualSalary))
-
-
 ## ----Paste--------------------------------------------------------------------
 paste("Visit", 1:5, sep = "_")
-paste("Visit", 1:5, sep = "_", collapse = " ")
+paste("Visit", 1:5, sep = "_", collapse = "_")
 paste("To", "is going be the ", "we go to the store!", sep = "day ")
 # and paste0 can be even simpler see ?paste0 
-paste0("Visit",1:5)
+paste0("Visit",1:5) # no space!
 
 
 ## ----unite_df-----------------------------------------------------------------
 df = tibble(id = rep(1:5, 3), visit = rep(1:3, each = 5))
+head(df, 4)
 
 
 ## -----------------------------------------------------------------------------
-df %>% unite(col = "unique_id", id, visit, sep = "_")
-
-
-## ----unite_df2----------------------------------------------------------------
-df = tibble(id = rep(1:5, 3), visit = rep(1:3, each = 5))
-
-
-## -----------------------------------------------------------------------------
-df %>% unite(col = "unique_id", id, visit, sep = "_", remove = FALSE)
-
-
-## ----Paste2-------------------------------------------------------------------
-paste(1:5)
-paste(1:5, collapse = " ")
-
-
-## ----orderrank----------------------------------------------------------------
-sort(c("1", "2", "10")) #  not sort correctly (order simply ranks the data)
-order(c("1", "2", "10"))
-x = rnorm(10)
-x[1] = x[2] # create a tie
-rank(x)
+df_united <- df %>% unite(col = "unique_id", id, visit, sep = "_")
+head(df_united, 4)
 
 
 ## -----------------------------------------------------------------------------
@@ -431,4 +288,30 @@ dplyr_sal = dplyr_sal %>% mutate(
 check_Sal = Sal
 rownames(check_Sal) = NULL
 all.equal(check_Sal, dplyr_sal)
+
+
+## -----------------------------------------------------------------------------
+tab <- table(c(0, 1, 2, 3, 2, 3, 3, 2,2, 3),
+             c(0, 1, 2, 3, 2, 3, 3, 4, 4, 3),
+              useNA = "always")
+tab
+
+
+## -----------------------------------------------------------------------------
+tab_df = tibble(x = c(0, 1, 2, 3, 2, 3, 3, 2,2, 3),
+             y = c(0, 1, 2, 3, 2, 3, 3, 4, 4, 3))
+tab_df %>% count(x, y)
+
+
+## -----------------------------------------------------------------------------
+tab_df %>% 
+  count(x, y) %>% 
+  group_by(x) %>% mutate(pct_x = n / sum(n))
+
+
+## ---- message = FALSE---------------------------------------------------------
+library(scales)
+tab_df %>% 
+  count(x, y) %>% 
+  group_by(x) %>% mutate(pct_x = percent(n / sum(n)))
 
