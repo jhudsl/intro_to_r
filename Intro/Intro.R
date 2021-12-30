3a53a27810f006635778c2afe7b74a77496cdc19
Carrie Wright <cwrigh60@jhu.edu>
2021-12-29 17:26:43 -0700

    updating syllabus details and intro

--- Intro/Intro.R
+++ Intro/Intro.R
@@ -15,16 +15,16 @@ knitr::include_graphics("https://ca.slack-edge.com/T023TPZA8LF-U024F9G49S8-9861d
 knitr::include_graphics("https://ca.slack-edge.com/T023TPZA8LF-U0248RUHRC2-0ac3f9608641-512")
 
 
-## ---- fig.alt="Marta's picture", out.width = "30%", echo = FALSE, fig.align='center'----
-knitr::include_graphics("https://ca.slack-edge.com/T023TPZA8LF-U024EN26Q0L-113294823b2c-512")
+## ---- fig.alt="Candace's picture", out.width = "30%", echo = FALSE, fig.align='center'----
+knitr::include_graphics("https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-instructor-photos.s3.amazonaws.com/3d/20b2387e1f494290006afaddce852a/Candace_Savonen.jpg?auto=format%2Ccompress&dpr=2&w=112&h=112&q=40&fit=crop")
 
 
-## ---- fig.alt="Michael's picture", out.width = "30%", echo = FALSE, fig.align='center'----
+## ---- fig.alt="Grant's picture", out.width = "30%", echo = FALSE, fig.align='center'----
 knitr::include_graphics("https://ca.slack-edge.com/T9HRLUCSW-UHLJ37F7U-c7982c75f1d0-512")
 
 
-## ---- fig.alt="Michael's picture", out.width = "30%", echo = FALSE, fig.align='center'----
-knitr::include_graphics("https://ca.slack-edge.com/T9HRLUCSW-U024P27E4JV-dea9308f50ef-512")
+## ---- fig.alt="Qier's picture", out.width = "30%", echo = FALSE, fig.align='center'----
+knitr::include_graphics("https://ca.slack-edge.com/T9HRLUCSW-U017Y4E376F-afced08ed082-192")
 
 
 ## ---- fig.alt="Bell Labs old logo", out.width = "40%", echo = FALSE, fig.align='center'----
