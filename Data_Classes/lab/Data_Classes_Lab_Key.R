a36b14ba17fa0f029a22c07e4a1a5b0f0962571c
Carrie Wright <cwrigh60@jhu.edu>
2022-01-12 00:23:42 -0500

    adding updates to lab and lecture but mith need to reduce

--- Data_Classes/lab/Data_Classes_Lab_Key.R
+++ Data_Classes/lab/Data_Classes_Lab_Key.R
@@ -1,119 +1,82 @@
-## ---------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(readr)
 library(tidyverse)
 library(dplyr)
 library(lubridate)
 library(jhur)
 
-bike = read_csv(
-  "http://johnmuschelli.com/intro_to_r/data/Bike_Lanes.csv")
+
+## -----------------------------------------------------------------------------
+set.seed(1234)
+int_vect <- rep(seq(1, 10), 3)
+rand_vect <- sample( 1:30, size = 30, replace = TRUE)
+TF_vect <- rep(c(TRUE, TRUE, FALSE), times = 10)
+TF_vect2 <- rep(c("TRUE", "TRUE", "FALSE"), times = 10)
 
 
-## ---------------------------------------------------------------------------------
-library(jhur)
-bike = read_bike()
+## -----------------------------------------------------------------------------
+vect_data <- tibble(int_vect, rand_vect, TF_vect, TF_vect2)
 
 
-## ----q1---------------------------------------------------------------------------
-head(factor(bike$type))
-
-btypes = sort(unique(bike$type))
-x = c("SIDEPATH","BIKE BOULEVARD", "BIKE LANE", "CONTRAFLOW",
-        "SHARED BUS BIKE",  "SHARROW",  "SIGNED ROUTE")
-dput(btypes)
-dput(btypes)[c(6,1:5,7)]
-dput(btypes[c(6,1:5,7)])
-
-lev = c( "SIDEPATH", "BIKE BOULEVARD", "BIKE LANE", "CONTRAFLOW", "SHARED BUS BIKE", 
-        "SHARROW", "SIGNED ROUTE")
+## -----------------------------------------------------------------------------
+slice_sample(vect_data, n = 5)
+slice_sample(vect_data, n = 5)
+slice_sample(vect_data, n = 5)
 
 
-## ----q2---------------------------------------------------------------------------
-bike$type = factor(bike$type)
-bike$type = relevel(bike$type, "SIDEPATH")
-bike$type = factor(bike$type,
-          levels = dput(btypes[c(6,1:5,7)]))
-bike = bike %>% mutate(type = factor(type, 
-                levels = dput(btypes[c(6,1:5,7)])))
-
-table(bike$type)
+## -----------------------------------------------------------------------------
+is.logical(TF_vect)
+is.logical(TF_vect2)
 
 
-## ----q3---------------------------------------------------------------------------
-bike = bike %>% 
-  mutate(type2 = factor(type, 
-             levels = c( "SIDEPATH", "BIKE BOULEVARD", 
-                         "BIKE LANE") ) )
-table(bike$type)
-table(bike$type2)
-table(bike$type2, useNA = "always")
+## -----------------------------------------------------------------------------
+vect_data <- mutate(vect_data, type_fact = factor(int_vect))
+vect_data
 
 
-## ----q4a--------------------------------------------------------------------------
-bike = bike %>% 
-  mutate(dateInstalled = 
-           as.character(dateInstalled)
-  )
-head(bike$dateInstalled)
+## -----------------------------------------------------------------------------
+levels(vect_data$type_fact)
 
 
-## ----q4b--------------------------------------------------------------------------
-bike = bike %>% 
-  mutate(dateInstalled = 
-           factor(dateInstalled)
-  )
-head(bike$dateInstalled)
-
-table(factor(bike$dateInstalled, levels = 2005:2017))
-table(factor(bike$dateInstalled, levels = 2005:2017), 
-        useNA="ifany")
+## -----------------------------------------------------------------------------
+circ = read_circulator()
+str(circ)
 
 
-## ----q4c--------------------------------------------------------------------------
-head(as.numeric(bike$dateInstalled)) 
+## -----------------------------------------------------------------------------
+circ = mutate(circ, date_formatted = mdy(date))
 
 
-## ----q4d--------------------------------------------------------------------------
-head(as.numeric(as.character(
-    bike$dateInstalled)))
+## -----------------------------------------------------------------------------
+str(circ)
 
 
-## ----q5---------------------------------------------------------------------------
- 
-bike = bike %>% mutate(
-    type = as.character(type),
-    type2 = ifelse(type %in% c("CONTRAFLOW", "SHARED BUS BIKE", 
-                               "SHARROW", "SIGNED ROUTE"), "OTHER", type),
-    type2 = factor(type2, levels = c( "SIDEPATH", "BIKE BOULEVARD", 
-                               "BIKE LANE", "OTHER") ))
-
-table(bike$type2)
-
-bike2 = bike %>% 
-  mutate(
-    type = factor(type,
-                  levels = c( "SIDEPATH", "BIKE BOULEVARD", 
-                              "BIKE LANE", "CONTRAFLOW", 
-                              "SHARED BUS BIKE", 
-                              "SHARROW", "SIGNED ROUTE")
-                  ),
-    type2 = recode_factor(type, 
-                          "CONTRAFLOW" = "OTHER",
-                          "SHARED BUS BIKE" = "OTHER",
-                          "SHARROW" = "OTHER",
-                          "SIGNED ROUTE" = "OTHER")
-  )
-table(bike2$type2)
+## -----------------------------------------------------------------------------
+range(circ$date_formatted)
 
 
-## ----q6a--------------------------------------------------------------------------
-ymd("2014/02-14")
+## -----------------------------------------------------------------------------
+circ = mutate(circ, date_year = year(date_formatted))
 
 
-## ----q6b--------------------------------------------------------------------------
-mdy_hm("04/22/14 03:20")
+## -----------------------------------------------------------------------------
+table(circ$date_year)
 
 
-## ----q6c--------------------------------------------------------------------------
-mdy_hms("4/5/2016 03:2:22")
+## -----------------------------------------------------------------------------
+vals <- seq(from = 1, to = 16)
+vals
+
+
+## -----------------------------------------------------------------------------
+mat <- matrix(vals, nrow = 4)
+mat
+
+
+## -----------------------------------------------------------------------------
+mat + 100
+
+
+## -----------------------------------------------------------------------------
+mat + mat
 
