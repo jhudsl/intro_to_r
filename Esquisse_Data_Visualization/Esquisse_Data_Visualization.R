94f77e18d8d47cb28f217b8830d051d825a0086f
Carrie Wright <cwrigh60@jhu.edu>
2022-01-17 13:28:35 -0500

    adding more about gifs for contributors

---
+++ Esquisse_Data_Visualization/Esquisse_Data_Visualization.R
@@ -0,0 +1,26 @@
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+library(here)
+
+
+## ----esquisse-----------------------------------------------------------------
+#install.packages("esquisse")
+library(esquisse)
+
+
+## ---- eval = FALSE------------------------------------------------------------
+## esquisser(mtcars)
+
+
+## ---- fig.alt="starting a plot", out.width = "50%", echo = FALSE, fig.align='center'----
+knitr::include_graphics("images/start_a_plot.png")
+
+
+## ---- fig.alt="select variables", out.width = "50%", echo = FALSE, fig.align='center'----
+  knitr::include_graphics("images/variables.gif")
+
+
+
+## ---- fig.alt="select variables", out.width = "50%", echo = FALSE, fig.align='center'----
+  knitr::include_graphics("images/get_code.gif")
+
