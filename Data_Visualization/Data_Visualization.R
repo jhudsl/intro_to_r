ca07628eb26c69d68793f232594821ba38980262
Carrie Wright <cwrigh60@jhu.edu>
2022-01-18 17:54:51 -0500

    updating data viz lecture and lab through part 1 of lab

--- Data_Visualization/Data_Visualization.R
+++ Data_Visualization/Data_Visualization.R
@@ -10,56 +10,64 @@ library(tidyverse)
 library(jhur)
 
 
-## ----seed, comment="",echo=FALSE----------------------------------------------
-set.seed(3) 
+## ---- fig.alt="esquisse", out.width = "31%", echo = FALSE, fig.show='hold',fig.align='center'----
+knitr::include_graphics("https://pbs.twimg.com/media/DoaBCAwWsAEaz-y.png")
+
+
+## ---- fig.alt="ggplot2", out.width = "21%", echo = FALSE, fig.show='hold',fig.align='center'----
+knitr::include_graphics("https://d33wubrfki0l68.cloudfront.net/2c6239d311be6d037c251c71c3902792f8c4ddd2/12f67/css/images/hex/ggplot2.png")
+
 
 
 ## ---- echo = FALSE------------------------------------------------------------
-set.seed(1)
+set.seed(456)
+emo::ji("graph")  
+emo::ji("plus") 
+emo::ji("graph")
 
 
-## ---- fig.width=4, fig.height=3-----------------------------------------------
+
+## ---- echo = FALSE, fig.align='center'----------------------------------------
+include_graphics("img/tidy_data_ex1.png")
+
+
+## ---- echo = FALSE, fig.align='center', out.width="80%"-----------------------
+include_graphics("img/messy_data_ex1.png")
+
+
+## ----seed---------------------------------------------------------------------
+set.seed(3) 
 var_1 <- seq(from = 1, to = 30)
 var_2 <- rnorm(30)
-
-plot(x = var_1, y = var_2)
-
-
-## ---- fig.width=4, fig.height=3-----------------------------------------------
-# creating a data frame from the vectors we had 
-my_data = data.frame(var_1, var_2)
+my_data = tibble(var_1, var_2)
 my_data
 
 
-## ---- fig.width=5, fig.height=4-----------------------------------------------
-# use $ to access column from a data set 
-plot(x = my_data$var_1, y = my_data$var_2)
+## ---- eval = FALSE, class.source = "codeexample"------------------------------
+## library(ggplot2) # don't forget to load ggplot2
+## # This is not code but shows the general format
+## ggplot({data_to plot},  mapping = aes(x = {var in data to plot},
+##                                       y = {var in data to plot}))
 
 
-## ---- fig.width=5, fig.height=4-----------------------------------------------
-# use $ to access column from a data set 
-boxplot(x = my_data$var_1)
+## ---- fig.width=3, fig.height=2.5, fig.align='center', class.source = "codereg"----
+ggplot(my_data, mapping = aes(x = var_1, y = var_2))
 
 
-## ---- fig.width=4, fig.height=3-----------------------------------------------
-head(my_data, 3)   # to recall how "my_data" data frame looks like
-library(ggplot2)
+## ---- eval = FALSE, class.source = "codeexample"------------------------------
+## 
+## ggplot({data_to plot},  mapping = aes(x = {var in data to plot},
+##                                       y = {var in data to plot})) +
+##   geom_{type of plot}</div>
 
-ggplot(my_data, aes(x = var_1, y = var_2)) + 
+
+## ---- fig.width=4, fig.height=3, fig.align='center'---------------------------
+
+ggplot(my_data, mapping = aes(x = var_1, y = var_2)) + 
   geom_point() 
 
 
-## ---- fig.width=4, fig.height=3, eval = FALSE---------------------------------
-## ggplot(my_data, mapping = aes(x = var_1, y = var_2)) +
-##   geom_point()
-
-
-## ---- fig.width=4, fig.height=3, eval = FALSE---------------------------------
-## ggplot(my_data, mapping = aes(x = var_1, y = var_2)) +
-##   geom_point()
-
-
-## ---- fig.width=4, fig.height=3-----------------------------------------------
+## ---- fig.width=3, fig.height=2.5---------------------------------------------
 plt1 <- 
   ggplot(my_data, aes(x = var_1, y = var_2)) + 
   geom_point()
@@ -71,37 +79,87 @@ plt2 <-
 plt1; plt2 # to have 2 plots printed next to each other on a slide
 
 
-## ---- fig.width=4, fig.height=3-----------------------------------------------
-plt3 <- 
-  ggplot(my_data, aes(y = var_2)) + 
-  geom_boxplot()
-
-plt4 <- 
-  ggplot(my_data, aes(x = var_2)) + 
-  geom_histogram()
-
-plt3; plt4 # to have 2 plots printed next to each other on a slide
-
-
-## ---- fig.width=5, fig.height=3-----------------------------------------------
+## ---- fig.width=4, fig.height=3, fig.align='center'---------------------------
 ggplot(my_data, aes(x = var_1, y = var_2)) + 
   geom_point() + 
   geom_line()
 
 
-## ---- fig.width=5, fig.height=3-----------------------------------------------
+## ---- fig.width=5, fig.height=3, fig.align='center'---------------------------
 ggplot(my_data, aes(x = var_1, y = var_2)) + 
   geom_point(size = 5, color = "red", alpha = 0.5) + 
-  geom_line(size = 0.8, color = "brown", linetype = 2)
+  geom_line(size = 0.8, color = "black", linetype = 2)
 
 
-## ---- fig.width=5, fig.height=3-----------------------------------------------
+## ---- fig.width=5, fig.height=3, fig.align='center'---------------------------
 ggplot(my_data, aes(x = var_1, y = var_2)) + 
   geom_point(size = 5, color = "red", alpha = 0.5) + 
   geom_line(size = 0.8, color = "brown", linetype = 2) + 
-  labs(x = "My variable 1", y = "My variable 2",
-       title = "My plot title") + 
-  theme_bw(base_size = 12, base_family = "Times New Roman")
+  theme_dark()
+
+
+## ---- fig.width=5, fig.height=3, fig.align='center'---------------------------
+ggplot(my_data, aes(x = var_1, y = var_2)) + 
+  geom_point(size = 5, color = "red", alpha = 0.5) + 
+  geom_line(size = 0.8, color = "brown", linetype = 2) + 
+  theme_bw(base_size = 20, base_family = "Comic Sans MS")
+
+
+## ---- fig.width=5, fig.height=3, fig.align='center'---------------------------
+ggplot(my_data, aes(x = var_1, y = var_2)) + 
+  geom_point(size = 5, color = "red", alpha = 0.5) + 
+  geom_line(size = 0.8, color = "brown", linetype = 2) + 
+  labs(title = "My plot of var1 vs var2",
+           x = "Variable 1",
+           y = "Variable 2") + 
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+
+## ---- fig.width=5, fig.height=3, fig.align='center'---------------------------
+
+ggplot(my_data, aes(x = var_1, y = var_2)) + 
+  geom_point(size = 5, color = "red", alpha = 0.5) + 
+  geom_line(size = 0.8, color = "brown", linetype = 2) + 
+  labs(title = "My plot of var1 vs var2") + 
+  xlim(0,40)
+  
+
+
+## ---- fig.width=5, fig.height=3, fig.align='center'---------------------------
+seq(from = 0, to = 30, by = 5)
+ggplot(my_data, aes(x = var_1, y = var_2)) + 
+  geom_point(size = 5, color = "red", alpha = 0.5) + 
+  geom_line(size = 0.8, color = "brown", linetype = 2) + 
+  labs(title = "My plot of var1 vs var2") + 
+  scale_x_continuous(breaks = seq(from = 0, to = 30, by = 5))
+
+
+## ---- fig.width=5, fig.height=3, fig.align='center'---------------------------
+ggplot(my_data, aes(x = var_1, y = var_2)) + 
+  geom_point(size = 5, color = "red", alpha = 0.5) + 
+  geom_line(size = 0.8, color = "brown", linetype = 2) + 
+  labs(title = "My plot of var1 vs var2") + 
+  theme(plot.title = element_text(hjust = 0.5, size = 20))
+
+
+
+## ---- fig.width=5, fig.height=3, fig.align='center'---------------------------
+ggplot(my_data, aes(x = var_1, y = var_2)) + 
+  geom_point(size = 5, color = "red", alpha = 0.5) + 
+  labs(title = "My plot of var1 vs var2", x = "Variable 1") +
+  theme(plot.title = element_text(hjust = 0.5, size = 20),
+        axis.title.x = element_text(size = 16))
+
+
+## -----------------------------------------------------------------------------
+head(Orange, 3)
+
+
+## ---- fig.width=3, fig.height=2, fig.align='center'---------------------------
+ggplot(Orange, aes(x = Tree, y = circumference, fill = Tree)) + 
+  geom_boxplot() +
+  theme(legend.position = "none")
 
 
 ## -----------------------------------------------------------------------------
