adfd1f3f2b4cfb66a99808875f87977c1b58e75d
avahoffman <avamariehoffman@gmail.com>
2022-05-24 01:35:58 -0400

    Try gha approach to pdf building

--- scripts/render_modules.R
+++ scripts/render_modules.R
@@ -19,10 +19,9 @@ for (i in 1:length(lab_files)) {
                     output_dir = paste0("docs/modules/", module_name, "/lab"))
 }
 
-# loop thru and render all lab files to html and pdf (pagedown)
+# loop thru and render all lab files to html 
 # Specific module name will be pulled out based on the dir name in modules/
 for (i in 1:length(lecture_files)) {
   module_name <- stringr::str_split(lecture_files, pattern = '/')[[i]][2]
   rmarkdown::render(lecture_files[i], output_dir = paste0("docs/modules/", module_name))
-  pagedown::chrome_print(paste0("docs/modules/", module_name, "/index.html"))
 }
