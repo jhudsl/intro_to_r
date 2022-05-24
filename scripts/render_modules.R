d38df3e36b3f1b8f8d972a67b5b86b934e722a25
avahoffman <avamariehoffman@gmail.com>
2022-05-24 11:43:13 -0400

    Add file updater to GHA

--- scripts/render_modules.R
+++ scripts/render_modules.R
@@ -3,22 +3,13 @@
 # Find anything ending in Rmd
 files <- dir(pattern = '[.]Rmd$', recursive = TRUE)
 
-print(file.info(files))
-
-# Find only the Rmds that have been updated since last rendering
-last_rendered <- readr::read_rds(file = "docs/last_rendered_timestamp.rds")
-print(paste0("Last module rmarkdown render time was ", last_rendered))
-files <-
-  dplyr::filter(file.info(files), mtime > readr::read_rds(file = "docs/last_rendered_timestamp.rds"))
-filenames <- rownames(files)
-
 # Lab files should be in a /lab subdirectory within modules/
-lab_files <- filenames[grepl("modules/.*/lab", filenames)]
+lab_files <- files[grepl("modules/.*/lab", files)]
 
 # Lecture files are everything else in modules
 lecture_files <-
-  filenames[grepl("modules/.*", filenames) &
-              !grepl("modules/.*/lab", filenames)]
+  files[grepl("modules/.*", files) &
+              !grepl("modules/.*/lab", files)]
 
 # loop thru and render all lab files to html
 # Specific module name will be pulled out based on the dir name in modules/
@@ -35,7 +26,3 @@ try(for (i in 1:length(lecture_files)) {
     stringr::str_split(lecture_files, pattern = '/')[[i]][2]
   rmarkdown::render(lecture_files[i], output_dir = paste0("docs/modules/", module_name))
 }, TRUE)
-
-# Save timestamp to avoid re-rendering all files next time
-last_rendered <- Sys.time()
-write_rds(last_rendered, file = "docs/last_rendered_timestamp.rds")
