05e993493563f3f469db6fba773700187de25028
avahoffman <avamariehoffman@gmail.com>
2022-05-24 15:00:55 -0400

    Tweak comments in src

--- scripts/render_module_details_for_syllabus.R
+++ scripts/render_module_details_for_syllabus.R
@@ -1,12 +1,12 @@
-# Render modules
+# Render module detail files
 
 # Find anything ending in Rmd
 files <- dir(pattern = '[.]Rmd$', recursive = TRUE)
 
-# Extract Cheatsheet files
+# Extract module detail files
 detail_files <- files[grepl("module_details", files)]
 
-# loop thru and render all cheatsheet files to pdf
+# loop thru and render all module detail files to html
 if (length(detail_files) != 0) {
   for (file in detail_files) {
     rmarkdown::render(file,
