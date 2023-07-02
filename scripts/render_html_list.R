9a3c31d027b307e30ca6810d85b459a853962114
avahoffman <avamariehoffman@gmail.com>
2023-07-02 09:41:22 -0700

    Take off file suffixes

--- scripts/render_html_list.R
+++ scripts/render_html_list.R
@@ -8,10 +8,11 @@ files <- readr::read_csv("lecture_files.csv")
 
 if(nrow(files) > 0) {
   files$files <- sapply(files$files, gsub, pattern = ".Rmd", replacement = "")
+  files$files <- sapply(files$files, gsub, pattern = "modules/.*/", replacement = "")
 } else {
   # Just render the Intro files if no others to render.
   files <- data.frame(
-    files = "modules/Intro/Intro"
+    files = "Intro"
   )
 }
 
