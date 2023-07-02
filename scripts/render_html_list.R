ee57169677de0a44bcceb256adc448c665bd97cb
avahoffman <avamariehoffman@gmail.com>
2023-07-02 09:13:23 -0700

    Fix naming convention

--- scripts/render_html_list.R
+++ scripts/render_html_list.R
@@ -7,7 +7,7 @@
 files <- readr::read_csv("lecture_files.csv")
 
 if(nrow(files) > 0) {
-  files$simple <- sapply(files$files, gsub, pattern = ".Rmd", replacement = "")
+  files$files <- sapply(files$files, gsub, pattern = ".Rmd", replacement = "")
 } else {
   # Just render the Intro files if no others to render.
   files <- data.frame(
