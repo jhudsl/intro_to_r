3e0d3ce8c6427f4ea254c578ce8264f3726e7a6f
avahoffman <avamariehoffman@gmail.com>
2024-03-15 14:29:04 -0400

    Add autogenerating backend

---
+++ scripts/blank_labs_from_keys.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+#awk 'BEGIN {RS="```"} NR%2==0 {gsub("}.*", "}\n\n")} NR>1 {printf "%s%s", prev_record, RS} {prev_record=$0} END {printf "%s", prev_record}' /Users/avahoffman/Downloads/Basic_R_Lab_Key.Rmd > modified_file.Rmd
+
+find modules/ -type f -name "*Lab_Key.Rmd" -print0 | while IFS= read -r -d '' file; do
+    # Remove Rmarkdown chunk content
+    # !! Note that the chunks must be named, with the name ending with "response" 
+    # to be cleared. This prevents unwanted erasure of library loading and knitr
+    # options. EG, a chunk set to be cleared would look like:
+    #```{r 1.1response}
+    #my.num <- c(5, 4, 7, 8, 12, 14) # These can be any real numbers
+    #```
+    modified_file="${file%_Key.Rmd}.Rmd"
+    awk 'BEGIN {RS="```"} NR%2==0 {gsub("response}.*", "response}\n\n")} NR>1 {printf "%s%s", prev_record, RS} {prev_record=$0} END {printf "%s", prev_record}' "$file" > "$modified_file"
+    echo created $modified_file
+
+    # Remove " - Key" from YAML header
+    sed -i '' 's/ - Key//' "$modified_file"
+done
